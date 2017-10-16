c4b430e2ed662fd422c1d2d4c1ca9f3e5730ec99
calmeida-fuze <calmeida@fuze.com>
2017-10-16 17:13:09 +0100

    Fix problem that affected the reset configuration on the windows version

--- src/app/js/resetData.js
+++ src/app/js/resetData.js
@@ -7,9 +7,10 @@ const pjson = remote.getGlobal('pjson')
 window.onload = function () {
 	console.debug("initialize Data: in window.onload " + 'bbbb');
 	settings.resetSettings(function(json) {
-		__dirname = __dirname.substring(0, __dirname.lastIndexOf('/'));
+		//__dirname = __dirname.substring(0, __dirname.lastIndexOf('/'));
 		var url = pjson.config.loginurl;
 		var url = url.substring(url.indexOf('/')+1);
-		mainWindow.loadURL(`file://${__dirname}/${url}`, {})
+
+		mainWindow.loadURL(`file://${__dirname}/../${url}`, {})
 	});
 }
