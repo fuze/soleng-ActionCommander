05c377bbd709901bfc20b5e016531bc3f40c3ea2
Isaiah <dragonspeakerweb@gmail.com>
2018-01-25 15:58:33 -0500

    Added recall of settings

--- src/app/js/settingsRender.js
+++ src/app/js/settingsRender.js
@@ -1,6 +1,5 @@
 const { ipcRenderer } = require('electron')
 const settings = require('electron-settings')
-console.log('something else entirely')
 window.onload = function () {
 	console.log("settings page loaded!")
   let cancleButton = document.getElementById("cancle-button")
@@ -36,25 +35,29 @@ function loadTriggerList(triggerPane){ //loads trigger list from settings
 }
 
 class TriggerRow {
-  constructor(stateChange,presenceValue,cmd) {
-  	this.stateChange = stateChange
-  	this.presenceValue = presenceValue
-  	this.cmd = cmd
+  constructor(values) {
+  	this.stateChange = values.stateChange
+  	this.presenceValue = values.presenceValue
+  	this.cmd = values.cmd
   }
 
-  createSelect(value, list){
+  createSelect(className, value, list){
   	let input = document.createElement('select')
+  	input.setAttribute('class', className)
   	for (var i in list){
   		let option = document.createElement('option')
   		option.setAttribute('value', list[i])
   		option.innerHTML = list[i]
-  		if (list[i] == value){input.selectedIndex = option.index} //Sets the default state of the dropdown
+  		//if (list[i] == value){input.selectedIndex = option.index} //Sets the default state of the dropdown
+
   		input.appendChild(option)
   	}
+  	if (value){input.value = value}
   	return input
   }
-  createInput(value){
+  createInput(className, value){
   	let input = document.createElement('input')
+  	input.setAttribute('class', className)
   	input.setAttribute('type', 'text')
   	if (value !== undefined){
   		input.setAttribute('value', value)
@@ -65,9 +68,9 @@ class TriggerRow {
   get createElement() {
   	let row = document.createElement('div')
   	row.setAttribute('class', 'trigger-row')
-  	let stateChangeInput = this.createSelect(this.stateChange,["to","from"])
-  	let presenceValueInput = this.createSelect(this.presenceValue,["available","busy"]) //add more values
-  	let cmdInput = this.createInput(this.cmd)
+  	let stateChangeInput = this.createSelect("stateChange", this.stateChange, ["to","from"])
+  	let presenceValueInput = this.createSelect("presenceValue", this.presenceValue, ["available","busy"]) //add more values
+  	let cmdInput = this.createInput("cmd", this.cmd)
   	
   	row.appendChild(stateChangeInput)
   	row.appendChild(presenceValueInput)
@@ -86,5 +89,19 @@ function addTriggerRow(triggerPane, values){ //adds row of UI elements to the en
 
 
 function getTriggerList(){ //reads the list of triggers from the UI and returns an array
+	let triggerList = []
+	let triggerPane = document.getElementById("trigger-pane")
+	let rowList = triggerPane.children
+	for (var i=0; i<rowList.length; i++){  //save each triggerRow in the UI as an object in the triggerList
+		let thisRow = rowList.item(i)
+		let tirggerObject = {}
+		tirggerObject.stateChange = thisRow.querySelector('.stateChange').value
+		tirggerObject.presenceValue = thisRow.querySelector('.presenceValue').value
+		tirggerObject.cmd = thisRow.querySelector('.cmd').value
+		if (tirggerObject.cmd){
+			triggerList.push(tirggerObject)
+		}
+	}
+	return triggerList
 
 }
