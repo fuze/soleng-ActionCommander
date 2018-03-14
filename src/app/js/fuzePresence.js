6d4e76087c1196e119be4a916fff03e6d9728f35
ifrias-fuze <ifrias@fuze.com>
2018-03-14 17:32:35 +0000

    Reformat..

--- src/app/js/fuzePresence.js
+++ src/app/js/fuzePresence.js
@@ -1,28 +1,32 @@
-const https = require("https")
-const url = require('url')
+const https = require("https");
+const url = require("url");
 
-module.exports = {}
-module.exports.getPresence = function getPresence(wardenToken, callback){
-  let options = url.parse('https://presence.fuze.com/api/v1/entities/me/presence?')
+function getPresence(wardenToken, callback) {
+  let options = url.parse(
+    "https://presence.fuze.com/api/v1/entities/me/presence?"
+  );
   options.headers = {
     Authorization: "Bearer " + wardenToken
-  }
+  };
 
-  
-  https.get(options, (response) => {
-  	response.setEncoding("utf8");
- 		let body = "";
-  	response.on("data", data => {
-    	body += data;
-  	});
-		response.on("end", () => {
-			body = JSON.parse(body)
-	  	const { status } = body
-  		if (status !== 200){
-  		  callback (status,body)
-			} else {
-  			callback(null,body)
-			}
-		})
-  })
+  https.get(options, response => {
+    response.setEncoding("utf8");
+    let body = "";
+    response.on("data", data => {
+      body += data;
+    });
+    response.on("end", () => {
+      body = JSON.parse(body);
+      const { status } = body;
+      if (status !== 200) {
+        callback(status, body);
+      } else {
+        callback(null, body);
+      }
+    });
+  });
+};
+
+module.exports = {
+	getPresence,
 }
