74ef7b736008eff0d9ed84beb3022ee16f37736b
ifrias-fuze <ifrias@fuze.com>
2018-03-20 19:35:46 +0000

    How to use PresenceWatcher

--- src/app/views/main/main.js
+++ src/app/views/main/main.js
@@ -1,20 +1,17 @@
 const { ipcRenderer } = require('electron');
 const settings = require("electron-settings");
-const presenceClient = require('soleng-presence-client');
+const { PresenceWatcher } = require('soleng-presence-client');
 const cjson = require('../../../config/config.json');
-
 const statusLabel = document.getElementById('status');
 
 function handlePresenceUpdate(status, result) {
-
-  //1. Check current-settings to see 
-  
-
+  //1. Check current-settings to see what action to take according to the previous and current presence
   if (status) {
     console.log('Received a status message ' + status);
   }
 
   if (result) {
+    console.log(JSON.stringify(result));
     statusLabel.innerHTML = 'New status : ' + result.status.presence;
   }
 }
@@ -29,5 +26,7 @@ function pushDataToConfObject(confObject, authDetails) {
 
 ipcRenderer.on('contents-loaded', (event, data) => {
   let configuration = pushDataToConfObject(cjson, data);
-  presenceClient.start(configuration, handlePresenceUpdate);
+  const presenceWatcher = new PresenceWatcher.PresenceWatcher(cjson, 5000);
+  presenceWatcher.start(handlePresenceUpdate);
 });
+
