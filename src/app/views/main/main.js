04c953db51eb094547084456d40347039d2770fe
ifrias-fuze <ifrias@fuze.com>
2018-03-21 10:22:41 +0000

    Display call events as a separate label

--- src/app/views/main/main.js
+++ src/app/views/main/main.js
@@ -1,8 +1,9 @@
 const { ipcRenderer } = require('electron');
 const settings = require("electron-settings");
-const { PresenceWatcher } = require('soleng-presence-client');
+const { PresenceWatcher, PresenceUpdateRate, CallEventsWatcher } = require('soleng-presence-client')
 const cjson = require('../../../config/config.json');
 const statusLabel = document.getElementById('status');
+const callStatusLabel = document.getElementById('call-status');
 
 function handlePresenceUpdate(status, result) {
   //1. Check current-settings to see what action to take according to the previous and current presence
@@ -11,8 +12,17 @@ function handlePresenceUpdate(status, result) {
   }
 
   if (result) {
-    console.log(JSON.stringify(result));
     statusLabel.innerHTML = 'New status : ' + result.status.presence;
+  }
+}
+
+function handleCallUpdate(status, result) {
+  if (status) {
+    console.log('Received a status message' + status);
+  }
+
+  if (result) {
+    callStatusLabel.innerHTML = 'New status : ' + result.status.presence;
   }
 }
 
@@ -26,7 +36,11 @@ function pushDataToConfObject(confObject, authDetails) {
 
 ipcRenderer.on('contents-loaded', (event, data) => {
   let configuration = pushDataToConfObject(cjson, data);
-  const presenceWatcher = new PresenceWatcher.PresenceWatcher(cjson, 5000);
+
+  const presenceWatcher = new PresenceWatcher(cjson, PresenceUpdateRate);
   presenceWatcher.start(handlePresenceUpdate);
+
+  const callEventsWatcher = new CallEventsWatcher(cjson);
+  callEventsWatcher.start(handleCallUpdate);
 });
 
