6a46b15f5893be1abe0faa906af1d61ba5b6591d
Isaiah <dragonspeakerweb@gmail.com>
2018-04-13 09:00:13 -0400

    Prevent triggereing on previous state if there is none

--- src/app/js/triggerManager.js
+++ src/app/js/triggerManager.js
@@ -38,7 +38,6 @@ class TriggerManager {
 		//then check to make sure that the value we are looking for is in the newState but not the oldState
 		//or vice versa
 		let newState = data.status.platformData.data.tags.concat(data.status.platformData.data.status)
-		let oldState = data.previousStatus.platformData.data.tags.concat(data.previousStatus.platformData.data.status)
 		for (trigger of this.presenceTriggers.to) {
 			if (newState.includes(trigger.triggerValue) && !(oldState.includes(trigger.triggerValue))){
 				try {
@@ -48,12 +47,15 @@ class TriggerManager {
 				}
 			}
 		}
-		for (trigger of this.presenceTriggers.from) {
-			if (oldState.includes(trigger.triggerValue) && !(newState.includes(trigger.triggerValue))){
-				try {
-					trigger.callback()
-				} catch(err) {
-					console.log("error exicuting trigger callback: " + err)
+		if (data.previousStatus){
+			let oldState = data.previousStatus.platformData.data.tags.concat(data.previousStatus.platformData.data.status)		
+			for (trigger of this.presenceTriggers.from) {
+				if (oldState.includes(trigger.triggerValue) && !(newState.includes(trigger.triggerValue))){
+					try {
+						trigger.callback()
+					} catch(err) {
+						console.log("error exicuting trigger callback: " + err)
+					}
 				}
 			}
 		}
